0dbcf8ad1b2ffc1b63933382b4f4fccfbfbd54ee
Van Tran <tmvan.1903@gmail.com>
2025-12-23 00:29:44 +0700

    reduce kernel logs, add adguard dns

--- hosts/ideahost/configuration.nix
+++ hosts/ideahost/configuration.nix
@@ -16,9 +16,15 @@
   boot.loader.limine.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.plymouth.enable = true;
+  boot.consoleLogLevel = 3;
+  boot.kernelParams = [ "quite" ];
 
   networking.hostName = "ideahost";
   networking.wireless.iwd.enable = true;
+  networking.nameservers = [
+    "94.140.14.14:5353#adguard-dns.io"
+    "94.140.15.15:5353#adguard-dns.io"
+  ];
 
   time.timeZone = "Asia/Ho_Chi_Minh";
 
@@ -39,6 +45,15 @@
   services.displayManager.ly = {
     enable = true;
     settings.xinitrc = null;
+  };
+
+  services.resolved = {
+    enable = true;
+    domains = [ "~." ];
+    fallbackDns = [
+      "1.1.1.1#one.one.one.one"
+      "1.0.0.1#one.one.one.one"
+    ];
   };
 
   programs.zsh.enable = true;
