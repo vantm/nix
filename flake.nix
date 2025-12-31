c6db5e3d3279fb6c594e2dc5a8cfcb34084d1864
Van Tran <tmvan.1903@gmail.com>
2025-12-31 23:36:05 +0700

    add example vm

--- flake.nix
+++ flake.nix
@@ -50,6 +50,31 @@
       # > ./scripts/switch <hostname>
       nixosConfigurations = {
         ideahost = mkHost "ideahost" "vantm";
+
+        vm = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ({ pkgs, ... }: {
+              boot.isContainer = false;
+
+              users.users.root.password = "root";
+              services.getty.autologinUser = "root";
+
+              system.stateVersion = outputs.stateVersion;
+
+              virtualisation.vmVariant = {
+                virtualisation.memorySize = 2048;
+                virtualisation.cores = 2;
+              };
+
+              imports = [ <nixpkgs/nixos/modules/virtualisation/qemu-vm.nix> ];
+
+              virtualisation.qemu.options = [
+                "-device virtio-vga"
+              ];
+            })
+          ];
+        };
       };
 
       # To apply home-manager configuration, run:
