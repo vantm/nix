e44ec0f76f8cd7a82ccafa6078317b5dafe4062a
Van Tran <tmvan.1903@gmail.com>
2025-12-31 12:58:20 +0700

    add hardward for ideahost

--- hosts/ideahost/configuration.nix
+++ hosts/ideahost/configuration.nix
@@ -3,7 +3,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hosts/${hostname}/hardware-configuration.nix
+      ./hardware-configuration.nix
     ];
 
   system.stateVersion = outputs.stateVersion;
