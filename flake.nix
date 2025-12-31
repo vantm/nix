05571534631b6d008371fdb994d7905491e52c41
Van Tran <tmvan.1903@gmail.com>
2025-12-31 14:41:47 +0700

    format

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "My Personal NixOS";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -10,10 +10,10 @@
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
+    { self
+    , nixpkgs
+    , home-manager
+    ,
     }@inputs:
     let
       inherit (self) outputs;
@@ -22,20 +22,20 @@
       mkHost = hostname: username:
         nixpkgs.lib.nixosSystem {
           modules = [ ./hosts/${hostname}/configuration.nix ];
-          specialArgs = { 
+          specialArgs = {
             inherit outputs hostname username;
           };
         };
       mkHome = hostname: username:
         home-manager.lib.homeManagerConfiguration {
-         pkgs = import inputs.nixpkgs {
-         inherit system;
-           config.allowUnfreePredicate = pkg:
-             builtins.elem (inputs.nixpkgs.lib.getName pkg) [
-               "vscode"
-               "spotify"
-               "rider"
-             ];
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            config.allowUnfreePredicate = pkg:
+              builtins.elem (inputs.nixpkgs.lib.getName pkg) [
+                "vscode"
+                "spotify"
+                "rider"
+              ];
           };
           modules = [ ./home/home.nix ];
           extraSpecialArgs = {
@@ -50,9 +50,16 @@
       homeConfigurations = {
         ideahost = mkHome "ideahost" "vantm";
       };
+
+      stateVersion = "25.11";
+
       devShells.${system}.default = pkgs.mkShell {
         packages = [ home-manager.packages.${system}.home-manager ];
       };
-      stateVersion = "25.11";
+
+      apps.${system}.fmt = {
+        type = "app";
+        program = "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt";
+      };
     };
 }
